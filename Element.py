47b7c5cccc33b29dfa4ebcdc8671405532813eb4
Mathisro <147599709+Mathisro@users.noreply.github.com>
2023-10-11 13:47:28 +0200

    Defining classes 2

--- Element.py
+++ Element.py
@@ -13,9 +13,9 @@ class Element:
         self.EModul = elemRow[2] # E-modul
         self.tType = elemRow[3] # Tverrsnittype, 1 = I-profil og 2 = rundprofil
 
-        self.lenElem = np.sqrt((self.P1[0]-self.P2[0])**2 + (self.P1[1]-self.P2[1])**2)
+        self.lenElem = np.sqrt((self.P1[0]-self.P2[0])**2 + (self.P1[1]-self.P2[1])**2) # Finner lengden til elementene med Pytagoras
 
-    def rotMat(angle):
+    def rotMat(angle): # Definerer rotasjonsmatrisa for å transformere koordinatsystemene
         return np.array([np.array([np.cos(angle) , np.sin(angle) , 0, 0             , 0             , 0]),
                          np.array([-np.sin(angle), np.cos(angle) , 0, 0             , 0             , 0]),
                          np.array([0             , 0             , 1, 0             , 0             , 0]),
@@ -23,9 +23,9 @@ class Element:
                          np.array([0             , 0             , 0, -np.sin(angle), np.cos(angle) , 0]),
                          np.array([0             , 0             , 0, 0             , 0             , 1])])
 
-    def transformToGlobal(angle):
-        return np.matmul(rotMat(angle), V) # self.v? self.rotmat?
+    def transformFromGlobal(self, angle, array): # Definerer transformasjon fra globale til lokale koordinater
+        return np.matmul(self.rotMat(angle), array) # self.v? self.rotmat?
     
-    def transformFromGlobal(angle):
-        return np.matmul(np.transpose(rotMat(angle)), V)
+    def transformToGlobal(self, angle, array): # Definerer transformasjon fra globale til lokale koordinater
+        return np.matmul(np.transpose(self.rotMat(angle)), array)
     
