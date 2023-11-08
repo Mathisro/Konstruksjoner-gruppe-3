79e7d2b508bdebe8736074c99026fa763129a960
martinmehus <147600131+martinmehus@users.noreply.github.com>
2023-11-08 12:38:34 +0100

    lisinput ferdig

--- lesinput.py
+++ lesinput.py
@@ -1,62 +1,58 @@
 import csv
 import numpy as np
 
+
+#Inputfilen tar inn CSV-filer, der hver verdi er separert med ";". 
+#Returnerer lister på samme form som den utdelte koden er bygget opp for
 def lesinput(fil):
     # Åpner inputfilen
     with open(fil) as fil:
-        csv_reader = csv.reader(fil, delimiter = ';')
-        nodearray = [] #Definerer de ulike arrayene
+        csv_reader = csv.reader(fil, delimiter = ';') 
+        #Definerer de ulike arrayene som skal fylles med data
+        nodearray = [] 
         beamarray = []
         lastarray = []
         geoarray = []
-        geoCount = 0
-        #print("Header utført")
+        geoCount = 0 #initialiserer starten av geometri-arrayen
         
         
-        for row in csv_reader:
+        
+        for row in csv_reader: #
 
            
-            if (row[0])[-5:]=='Nodes': #Leser av første linje
+            if (row[0])[-5:]=='Nodes': #Definerer antall punkter og laster, angitt på samme linje som ordet "Nodes" i excel-fila
                 npunkt= int(row[1])
                 nlast = int(row[3])
-                #print("First row reading")
                 
-            elif (row[0])[-5:]=='Beams': #Leser av andre linje
+                
+            elif (row[0])[-5:]=='Beams': #Leser av antall bjelkeelementer, angitt etter ordet "Beams" i excel-fila
                 nelem = int(row[1])
-                #ntverr = int(row[3])
-                #print("second row reading")
+                
 
-            elif (row[0]=='Node'): #Leser av alle linjer med "Node"
+            elif (row[0]=='Node'): #Leser av alle linjer med der ordet "Node" står i første celle
                 idTemp = int(row[1])
                 xTemp = float(row[2])
                 yTemp = float(row[3])
                 fxTemp = float(row[4])
                 fyTemp = float(row[5])
                 mTemp = float(row[6])
-                tempMatrise = np.array([xTemp, yTemp, (fxTemp,fyTemp,mTemp)], dtype=object)
-                nodearray.append(tempMatrise)
-                #print("Node reading")
+                tempMatrise = np.array([xTemp, yTemp, (fxTemp,fyTemp,mTemp)], dtype=object) 
+                nodearray.append(tempMatrise) #Fyller knutepunktslista med knutepunktverdiene fra excel-arket
+                
 
-            elif (row[0]=='Beam'): #Leser av alle linjer med "Beam"
+            elif (row[0]=='Beam'): #Leser av alle linjer med der ordet "Beam" står i første celle
                 idTemp = int(row[1])
                 node1Temp = int(row[2])
                 node2Temp = int(row[3])
                 geoTemp = int(row[4])
                 flytspenning = float(row[5])
                 beamArrayTemp = np.array([idTemp,node1Temp,node2Temp,geoTemp,flytspenning])
-                beamarray.append(beamArrayTemp)
-                #print("Beam reading")
+                beamarray.append(beamArrayTemp) #Fyller bjelke-lista med bjelkeverdiene fra excel-arket
+                
               
-            #type: 1 hvis punktlast, 2 hvis fordelt last 
-            #intensitet
-            #startpunkt
-            #sluttpunkt, (null hvis punktlast)
-            #retning (vinkel)
-            #type fordelt last (1-7)
-            elif ((row[0])[:3]=='Pun' or (row[0])[:3]=='For'): #Leser av alle linjer med "Punktlast..." eller "Fordelt last..."
-                #idTemp = int(row[1])
-                #nodeIdTemp = int(row[2])
-                typeTemp = int(row[1])
+           
+            elif ((row[0])[:3]=='Pun' or (row[0])[:3]=='For'): #Leser av alle linjer med der første celle er "Punktlast..." eller "Fordelt last..."
+                typeTemp = int(row[1]) 
                 vinkelTemp = float(row[6])
                 intensTemp = float(row[7])
                 x1Temp = float(row[2])
@@ -64,13 +60,13 @@ def lesinput(fil):
                 x2Temp = float(row[4])
                 y2Temp = float(row[5])
                 lastArrayTemp = np.array([typeTemp, x1Temp, y1Temp, x2Temp, y2Temp, vinkelTemp, intensTemp])
-                lastarray.append(lastArrayTemp)
-                #print("Last reading")
+                lastarray.append(lastArrayTemp)  #Fyller last-lista med lastverdier fra excel-arket
+                
                 
 
-            elif ((row[0])[:3]=='Geo'): #indikerer at man er i nederste tabell
+            elif ((row[0])[:3]=='Geo'): #indikerer at man er i nederste tabell, der geometri-informasjon finnes
                 geoCount = 1
-                #print("start nedre tabell")
+                
 
             elif (geoCount ==1 and row[0]!= ''): #kjører ut nederste tabell
                 geoTemp = int(row[1])
@@ -85,18 +81,13 @@ def lesinput(fil):
                 mat_typeTemp = int(row[10])
                 ETemp = float(row[11])
                 geoArrayTemp = np.array([geoTemp, BTemp,bTemp,HTemp,hTemp,Rtemp,rTemp,t_stegTemp,t_flensTemp ,mat_typeTemp ,ETemp])
-                geoarray.append(geoArrayTemp) 
-                #print("geometry reading")
+                geoarray.append(geoArrayTemp) #Fyller geometri-lista med geometriverdier fra excelarket 
+                
 
 
             else :
                 continue 
     
     
-    return npunkt,nodearray, nelem, beamarray, nlast, lastarray, geoarray
-
-
-#for i in range(8): 
-    #print(values[i])
-
-#print(values[])
+    #Returnerer antall knutepunkter, knutepunktslista, antall bjelkeelementer, bjelkelista, antall laster, lastlista og geometrilista
+    return npunkt,nodearray, nelem, beamarray, nlast, lastarray, geoarray 
